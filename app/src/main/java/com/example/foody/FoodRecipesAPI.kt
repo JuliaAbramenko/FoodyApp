a3c5235d60250bf5dc193ef7ae84a7d8122fef9b
Julia Abramenko <julia.abramenko@protonmail.com>
2023-05-17 14:56:43 +0200

    Added complexSearch API request

---
+++ app/src/main/java/com/example/foody/FoodRecipesAPI.kt
@@ -0,0 +1,15 @@
+package com.example.foody
+
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesAPI {
+
+    @GET("/recipes/complexSearch")
+    //suspend for Co-routines
+    suspend fun getRecipes(
+        @QueryMap queries : Map<String,String>
+    ) : Response<FoodRecipe>
+}
