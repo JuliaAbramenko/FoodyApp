f5e00b1efa196efb03a316b4de7a96c0d3600b63
Julia Abramenko <julia.abramenko@protonmail.com>
2023-05-22 09:17:11 +0200

    Injection in RemoteDataSource

--- app/src/main/java/com/example/foody/RemoteDataSource.kt
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.example.foody
 
-class RemoteDataSource {
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesAPI: FoodRecipesAPI
+) {
+
+    suspend fun getRecipes(queries: Map<String,String>) : Response<FoodRecipe>{
+        return foodRecipesAPI.getRecipes(queries)
+    }
 }
